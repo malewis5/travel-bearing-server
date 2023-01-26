3434d23a754e4817fccb2c0d4bf6bb28fae23218
Matthew Lewis <matthew.a.lewis7@gmail.com>
2023-01-25 17:52:14 -0800

    init

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  var options = {
+    root: path.join(__dirname),
+  };
+
+  var fileName = '/library/test.ics';
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
